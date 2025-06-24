6639ac30009feb5ab54881638ecad6485eb3245f
aungkyaw02 <akthet2005@gmail.com>
2025-06-24 22:01:57 +0630

    layout system

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="app.css">
-</head>
-<body>
+@extends('layout')
+@section('title')
+{{ $blog->title }}
+@endsection
+@section('content')
+
     <article>
-      <h1><?= $blog->title ?></h1>
-      <p><?= $blog->body ?></p>
+        <h1><?= $blog->title ?></h1>
+        <p> {!! $blog->body !!} </p>
         <a href="/">go back</a>
     </article>
-</body>
-</html>
+
+@endsection
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,27 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="app.css">
-    <style>
-        .bg-yellow {
-            background-color: yellow;
-        }
-    </style>
-</head>
-<body>
+@extends('layout')
+@section('title', 'Blogs')
+@section('content')
+
     @foreach ($blogs as $blog)
     {{-- @dd($loop) --}}
-    <h1><a href="blogs/{{ $blog->slug }}"> 
-        {{ $blog->title }} </a>
-    </h1>
     <div>
-        <p> {{ $blog->intro  }} </p>
-        <p>published at: {{ $blog->date }} </p>
+        <h1><a href="blogs/{{ $blog->slug }}"> 
+        {{ $blog->title }} </a>
+        </h1>
+        <div>
+            <p> {{ $blog->intro  }} </p>
+            <p>published at: {{ $blog->date }} </p>
+        </div>
     </div>
     @endforeach
-</body>
-</html>
+
+@endsection
