4fd0e22b18f332eee71862c7134326da6690666e
aungkyawthetz <aungkyawthethimself@gmail.com>
2025-07-16 21:21:15 +0630

    comment design

---
+++ resources/views/components/single-comment.blade.php
@@ -0,0 +1,14 @@
+<div class="card d-flex p-3 my-3 shadow-sm">
+  <div class="d-flex">
+    <div>
+      <img src="https://i.pravatar.cc/300" alt="" width="50" height="50" class="rounded-circle">
+    </div>
+    <div class="ms-3">
+      <h6>Aung Kyaw Thet</h6>
+      <p class="text-secondary"> 4 months ago</p>
+    </div>
+  </div>
+  <p class="mt-1">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempore, vero nulla rerum cum incidunt aperiam, voluptatum vitae exercitationem ipsum numquam accusantium possimus veniam amet maiores ea, et explicabo similique?
+  </p>
+</div>
