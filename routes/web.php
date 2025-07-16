c2440272c4b159300e2876171153557554a7d539
aungkyawthetz <aungkyawthethimself@gmail.com>
2025-07-16 20:02:16 +0630

    middleware

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/blogs/{blog:slug}', [BlogController::class, 'show'])->where('blog',
 //     ]);
 // });
 
-Route::get('/register', [AuthController::class, 'create']);
-Route::post('/register', [AuthController::class, 'store']);
+Route::get('/register', [AuthController::class, 'create'])->middleware('guest');
+Route::post('/register', [AuthController::class, 'store'])->middleware('guest');
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/login', [AuthController::class, 'storeLogin']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/login', [AuthController::class, 'login'])->middleware('guest');
+Route::post('/login', [AuthController::class, 'storeLogin'])->middleware('guest');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
